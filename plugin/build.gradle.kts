ec1872f438672d66a129bd1029e47e9512c90fcf
otopba <otopba1@gmail.com>
2025-11-11 11:02:23 +0200

    version = project.findProperty("version")?.takeIf { it != "unspecified" } ?: "1.9"

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.raymank26"
-version = project.findProperty("version")?.takeIf { it != "unspecified" } ?: "1.8"
+version = project.findProperty("version")?.takeIf { it != "unspecified" } ?: "1.9"
 
 repositories {
     mavenCentral()
