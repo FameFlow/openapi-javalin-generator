c63c271a654620eb1c98c2bc570b490921b59814
otopba <otopba1@gmail.com>
2025-11-11 09:52:20 +0200

    runnable fix 4

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.raymank26"
-version = project.findProperty("version")?.takeIf { it != "unspecified" } ?: "1.5"
+version = project.findProperty("version")?.takeIf { it != "unspecified" } ?: "1.6"
 
 repositories {
     mavenCentral()
